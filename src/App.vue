52138360719aa8fb2b6b8f39ac70538e89393aba
christianarca27 <christian.arca.mail@gmail.com>
2023-03-30 19:15:54 +0200

    feat: inserito AppHeader in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 import { store } from './store.js';
 
 export default {
   data() {
     return {
       store,
-    }
+    };
   },
+  components: {
+    AppHeader
+  }
 }
 </script>
 
 <template>
-  {{ store.searchText }}
+  <AppHeader></AppHeader>
 </template>
 
 <style lang="scss" scoped></style>
