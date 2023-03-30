093baf33479bb13772aac32e66cda3507379eb6f
christianarca27 <christian.arca.mail@gmail.com>
2023-03-30 19:15:38 +0200

    feat: in AppHeader, corretto import del global state + aggiunto nome componente + inserite barra di ricerca e bottone con collegamento alla funzione di avvio ricerca

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,40 @@
 <script>
-import store from './store.js';
+import { store } from '../store.js';
 
 export default {
+    name: "AppHeader",
+
     data() {
         return {
             store,
         }
     },
+
+    methods: {
+        search() {
+            console.log(this.store.searchText);
+        },
+    },
 }
 </script>
 
-<template></template>
+<template>
+    <div id="search">
+        <input @keyup.enter="search()" v-model="store.searchText" type="text" placeholder="Inserisci testo...">
+        <button @click="search()">Cerca</button>
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#search {
+    input {
+        padding: .5rem;
+        font-size: 1.2rem;
+    }
+
+    button {
+        padding: .5rem;
+        font-size: 1.2rem;
+    }
+}
+</style>
