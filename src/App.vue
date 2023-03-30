4fe33d99527938384c2382d43b0fb239c281532b
christianarca27 <christian.arca.mail@gmail.com>
2023-03-30 19:36:03 +0200

    feat: trasferita la funzione di ricerca da AppHeader a App tramite emit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import { store } from './store.js';
+import axios from 'axios';
 
 export default {
+  name: "App",
+
   data() {
     return {
       store,
     };
   },
+
   components: {
     AppHeader
-  }
+  },
+
+  methods: {
+    search() {
+      console.log("emit");
+    },
+  },
 }
 </script>
 
 <template>
-  <AppHeader></AppHeader>
+  <AppHeader @search="search()"></AppHeader>
 </template>
 
 <style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -6,4 +6,10 @@ export const store = reactive({
     movies: [],
 
     series: [],
+
+    queryBasicUrl: "https://api.themoviedb.org/3/",
+
+    APIkey: "?api_key=c0ba2ef7e9812c1f27d4220dc7f1ea71",
+
+    queryLang: "&language=it",
 });
