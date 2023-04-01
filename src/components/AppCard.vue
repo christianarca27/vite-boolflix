e4bdb817ee159a78824002e94b7f7029f704154d
christianarca27 <christian.arca.mail@gmail.com>
2023-04-01 15:03:17 +0200

    feat: unificata struttura AppCard + stilizzazione in modo che i dettagli siano visibili solo all'hover

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -22,83 +22,56 @@ export default {
 </script>
 
 <template>
-    <ul v-if="type == 'movie'" class="card-container movie-details">
-        <li class="card-poster">
+    <div class="card-wrapper">
+        <div class="card-poster">
             <img v-if="card.poster_path != null" :src="'https://image.tmdb.org/t/p/original' + card.poster_path" alt="">
 
             <div v-else class="poster-not-avaible">
                 <p>Poster non disponibile</p>
             </div>
-        </li>
-
-        <li class="card-title">
-            <h3>{{ card.title }}</h3>
-        </li>
-
-        <li class="card-original-title">
-            <small>{{ card.original_title }}</small>
-        </li>
-
-        <div class="card-original-language">
-            <p>Lingua originale: </p>
-            <lang-flag :iso="card.original_language" />
         </div>
 
-        <li class="card-vote-average">
-            <p>Voto:</p>
-            <i v-for="(star, index) in 5" :class="index < parseInt(card.vote_average / 2) ? 'fa-solid' : 'fa-regular'"
-                class="fa-star"></i>
-        </li>
-    </ul>
+        <ul class="card-details">
+            <li class="card-title">
+                <strong v-if="type == 'movie'">{{ card.title }}</strong>
+                <strong v-else>{{ card.name }}</strong>
+            </li>
 
-    <ul v-else class="card-container serie-details">
-        <li class="card-poster">
-            <img v-if="card.poster_path != null" :src="'https://image.tmdb.org/t/p/original' + card.poster_path" alt="">
+            <li class="card-original-title">
+                <small v-if="type == 'movie'">{{ card.original_title }}</small>
+                <small v-else>{{ card.original_name }}</small>
+            </li>
 
-            <div v-else class="poster-not-avaible">
-                <p>Poster non disponibile</p>
+            <div class="card-original-language">
+                <h3>Lingua originale: </h3>
+                <lang-flag :iso="card.original_language" />
             </div>
-        </li>
 
-        <li class="card-title">
-            <h3>{{ card.name }}</h3>
-        </li>
+            <li class="card-vote-average">
+                <h3>Voto:</h3>
+                <i v-for="(star, index) in 5" :class="index < parseInt(card.vote_average / 2) ? 'fa-solid' : 'fa-regular'"
+                    class="fa-star"></i>
+            </li>
 
-        <li class="card-original-title">
-            <small>{{ card.original_name }}</small>
-        </li>
-
-        <div class="card-original-language">
-            <p>Lingua originale: </p>
-            <lang-flag :iso="card.original_language" />
-        </div>
-
-        <li class="card-vote-average">
-            <p>Voto:</p>
-            <i v-for="(star, index) in 5" :class="index < Math.ceil(card.vote_average / 2) ? 'fa-solid' : 'fa-regular'"
-                class="fa-star"></i>
-        </li>
-    </ul>
+            <li class="card-overview">
+                <h3>Overview</h3>
+                <p>{{ card.overview }}</p>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-.card-container {
-    border: 1px solid white;
-
-    list-style-type: none;
-
-    i {
-        color: goldenrod;
-    }
-
-    li {
-        margin-bottom: .5rem;
-    }
+.card-wrapper {
+    width: calc(100% / 5 - 10px * 4 / 5);
+    // margin-right: 1rem;
+    cursor: pointer;
 
     .card-poster {
+        width: 100%;
 
         img {
-            width: 250px;
+            width: 100%;
             aspect-ratio: 2 / 3;
 
             object-fit: cover;
@@ -106,7 +79,7 @@ export default {
 
         .poster-not-avaible {
             background-color: black;
-            width: 250px;
+            width: 100%;
             aspect-ratio: 2 / 3;
 
             display: flex;
@@ -115,15 +88,49 @@ export default {
         }
     }
 
-    .flag-icon-undefined {
-        width: 0;
-        height: 0;
+    ul.card-details {
+        background-color: #141414;
+        display: none;
+        width: 100%;
+        aspect-ratio: 2 / 3;
+        padding: 1rem;
+        list-style-type: none;
+        overflow-y: auto;
 
-        &::before {
-            content: '\3f';
-            font-weight: bold;
-            font-size: 1.5rem;
+        li {
+            margin-bottom: .5rem;
         }
+
+        .card-title {
+            strong {
+                font-size: 1.5rem;
+            }
+        }
+
+        .card-original-language {
+            .flag-icon-undefined {
+                width: 0;
+                height: 0;
+
+                &::before {
+                    content: '\3f';
+                    font-weight: bold;
+                    font-size: 1.5rem;
+                }
+            }
+        }
+
+        .card-vote-average i {
+            color: goldenrod;
+        }
+    }
+
+    &:hover .card-poster {
+        display: none;
+    }
+
+    &:hover .card-details {
+        display: block;
     }
 }
 </style>
