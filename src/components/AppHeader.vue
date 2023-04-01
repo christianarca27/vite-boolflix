718592dab9b90bf93abbb98970fd47c89a0aca61
christianarca27 <christian.arca.mail@gmail.com>
2023-04-01 15:04:12 +0200

    feat: aggiunta logo + altre stilizzazioni header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,43 +17,75 @@ export default {
 </script>
 
 <template>
-    <div id="search">
-        <input @keyup.enter="$emit('perform-search')" v-model="store.searchText" type="text"
-            placeholder="Inserisci testo...">
-        <button @click="$emit('perform-search')">Cerca</button>
-    </div>
+    <header>
+        <div id="header-inner" class="container">
+            <div id="logo">
+                <h1>Boolflix</h1>
+            </div>
 
-    <div id="radio-button-group">
-        <div class="radio-button">
-            <input v-model="store.typeOfSearch" type="radio" name="type-of-search" value="both" id="type-of-search-both"
-                checked>
-            <label for="type-of-search-both">Tutto</label>
-        </div>
+            <div id="search">
+                <input @keyup.enter="$emit('perform-search')" v-model="store.searchText" type="text"
+                    placeholder="Inserisci testo...">
 
-        <div class="radio-button">
-            <input v-model="store.typeOfSearch" type="radio" name="type-of-search" value="movies"
-                id="type-of-search-movies">
-            <label for="type-of-search-movies">Film</label>
-        </div>
+                <div id="radio-button-group">
+                    <div class="radio-button">
+                        <input v-model="store.typeOfSearch" type="radio" name="type-of-search" value="both"
+                            id="type-of-search-both" checked>
+                        <label for="type-of-search-both">Tutto</label>
+                    </div>
 
-        <div class="radio-button">
-            <input v-model="store.typeOfSearch" type="radio" name="type-of-search" value="series"
-                id="type-of-search-series">
-            <label for="type-of-search-series">Serie</label>
+                    <div class="radio-button">
+                        <input v-model="store.typeOfSearch" type="radio" name="type-of-search" value="movies"
+                            id="type-of-search-movies">
+                        <label for="type-of-search-movies">Film</label>
+                    </div>
+
+                    <div class="radio-button">
+                        <input v-model="store.typeOfSearch" type="radio" name="type-of-search" value="series"
+                            id="type-of-search-series">
+                        <label for="type-of-search-series">Serie</label>
+                    </div>
+                </div>
+
+                <button @click="$emit('perform-search')">Cerca</button>
+            </div>
         </div>
-    </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
-#search {
-    input {
-        padding: .5rem;
-        font-size: 1.2rem;
+#header-inner {
+    display: flex;
+    justify-content: space-between;
+    // align-items: center;
+
+    #logo {
+        h1 {
+            color: red;
+            font-size: 4rem;
+        }
     }
 
-    button {
-        padding: .5rem;
-        font-size: 1.2rem;
+    #search {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        input {
+            padding: .5rem;
+            margin-right: .5rem;
+            font-size: 1.2rem;
+        }
+
+        #radio-button-group {
+            display: flex;
+            gap: 1rem;
+        }
+
+        button {
+            padding: .5rem;
+            font-size: 1.2rem;
+        }
     }
 }
 </style>
