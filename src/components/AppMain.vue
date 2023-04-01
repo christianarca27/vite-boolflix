f07226bcc4b0bce31581fff36a3b45e4ff5d5656
christianarca27 <christian.arca.mail@gmail.com>
2023-04-01 15:04:38 +0200

    feat: cambiate disposizioni + stilizzazione main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -25,45 +25,51 @@ export default {
 </script>
 
 <template>
-    <div v-show="store.isMoviesReady" id="movies-container">
-        <h2>Movies</h2>
+    <main>
+        <div class="container">
+            <div v-show="store.isMoviesReady" id="movies-container">
+                <h2>Film</h2>
 
-        <div class="cards-container">
-            <AppCard v-for="movie in store.movies" :type="'movie'" :card="movie"></AppCard>
+                <div class="page-status">
+                    <p>Pagina {{ this.store.actualMoviesPage }} / {{ this.store.totalMoviesPages }}</p>
+
+                    <button @click="$emit('prev-movies-page-search')">Prev Page</button>
+                    <button @click="$emit('next-movies-page-search')">Next Page</button>
+                </div>
+
+                <div class="cards-container">
+                    <AppCard v-for="movie in store.movies" :type="'movie'" :card="movie"></AppCard>
+                </div>
+            </div>
+
+            <div v-show="store.isSeriesReady" id="series-container">
+                <h2>Serie TV</h2>
+
+                <div class="page-status">
+                    <p>Pagina {{ this.store.actualSeriesPage }} / {{ this.store.totalSeriesPages }}</p>
+
+                    <button @click="$emit('prev-series-page-search')">Prev Page</button>
+                    <button @click="$emit('next-series-page-search')">Next Page</button>
+                </div>
+
+                <div class="cards-container">
+                    <AppCard v-for="serie in store.series" :type="'serie'" :card="serie"></AppCard>
+                </div>
+            </div>
+
+            <div v-show="!store.isMoviesReady || !store.isSeriesReady" id="loading-page">
+                <p>Download titoli in corso...</p>
+            </div>
         </div>
-
-        <div class="page-status">
-            <p>{{ this.store.actualMoviesPage }} / {{ this.store.totalMoviesPages }}</p>
-
-            <button @click="$emit('prev-movies-page-search')">Prev Page</button>
-            <button @click="$emit('next-movies-page-search')">Next Page</button>
-        </div>
-    </div>
-
-    <div v-show="store.isSeriesReady" id="series-container">
-        <h2>Series</h2>
-
-        <div class="cards-container">
-            <AppCard v-for="serie in store.series" :type="'serie'" :card="serie"></AppCard>
-        </div>
-
-        <div class="page-status">
-            <p>{{ this.store.actualSeriesPage }} / {{ this.store.totalSeriesPages }}</p>
-
-            <button @click="$emit('prev-series-page-search')">Prev Page</button>
-            <button @click="$emit('next-series-page-search')">Next Page</button>
-        </div>
-    </div>
-
-    <div v-show="!store.isMoviesReady || !store.isSeriesReady" id="loading-page">
-        <p>Download titoli in corso...</p>
-    </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
 .cards-container {
     display: flex;
-    flex-flow: row nowrap;
+
+    flex-flow: row wrap;
+    gap: 10px;
 
     overflow-x: auto;
 }
